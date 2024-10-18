0db92a9bbe5a896e63fe506613494ddef1f03477
Kartik <kartikmahar5@gmail.com>
2024-10-18 08:36:00 +0530

    dom1

---
+++ dom.js
@@ -0,0 +1,11 @@
+// window.alert("please login")
+let ans=window.confirm("confirm uh are 18 year old")
+document.write(ans)
+let name=window.prompt("What is ur name:");
+document.write(`your name is ${name}`)
+
+function message(){
+    window.open("https://www.youtube.com/")
+    window.close();
+    window.history();               //windos.histort() opens previous of the browser
+}
