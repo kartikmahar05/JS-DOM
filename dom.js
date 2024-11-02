e11277d21f8db72102202bd9ca687028e6df0adc
KISHOR  SINGH <133872880+kartikmahar05@users.noreply.github.com>
2024-11-02 09:34:30 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,7 +1,7 @@
 // window.alert("please login")
-let ans=window.confirm("confirm uh are 18 year old")
+let ans=window.confirm("confirm you are 18 year old")
 document.write(ans)
-let name=window.prompt("What is ur name:");
+let name=window.prompt("What is you name:");
 document.write(`your name is ${name}`)
 
 function message(){
